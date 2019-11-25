e6db3d75f1ae1423c693d2b28ea0ab6a67737919
BillAggelo <56355628+BillAggelo@users.noreply.github.com>
2019-11-25 21:03:56 +0200

    UI Testing Verision 1.1
    Made test for WeatherList Conditions on Menu items.

--- app/src/androidTest/java/com/example/farmweather/startTest.java
+++ app/src/androidTest/java/com/example/farmweather/startTest.java
@@ -1,5 +1,7 @@
 package com.example.farmweather;
 
+import android.widget.PopupMenu;
+
 import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.After;
@@ -9,16 +11,20 @@ import org.junit.Test;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.pressMenuKey;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.swipeDown;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsAnything.anything;
 
 public class startTest {
@@ -121,6 +127,55 @@ public class startTest {
         Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         Espresso.onView(withText("Καθαρισμός όλων")).perform(click());
         Espresso.onView(withText("ΝΑΙ")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+        Espresso.onView(isRoot()).perform(pressBack());
+        Espresso.onView(withId(R.id.list_town)).perform(click());
+        Espresso.onData(anything()).inAdapterView(withId(R.id.ListView)).atPosition(0).perform(longClick());
+        Espresso.onView(withText("ΝΑΙ")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+        Espresso.onView(isRoot()).perform(pressBack());
+        Espresso.onView(isRoot()).perform(pressBack());
+        Espresso.onView(withId(R.id.GetTown)).perform(typeText(Town));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.enter)).perform(click());
+        Espresso.onView(withId(R.id.list_town)).perform(scrollTo(),click());
+        Espresso.onData(anything()).inAdapterView(withId(R.id.ListView)).atPosition(0).perform(longClick());
+        Espresso.onView(withText("ΝΑΙ")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+    }
+
+    @Test
+    public void testListWeatherConditions(){
+        Espresso.onView(withId(R.id.GetTown)).perform(typeText(Town));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.enter)).perform(click());
+        Espresso.onView(withId(R.id.Row_add)).perform(scrollTo(),click());
+        Espresso.onView(withText("ΝΑΙ")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+        Espresso.onView(isRoot()).perform(pressBack());
+        Espresso.onView(withId(R.id.GetTown)).perform(typeText("Nigrita"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.enter)).perform(click());
+        Espresso.onView(withId(R.id.Row_add)).perform(scrollTo(),click());
+        Espresso.onView(withText("ΝΑΙ")).inRoot(isDialog()).check(matches(isDisplayed())).perform(click());
+        Espresso.onView(withId(R.id.list_town)).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Καθαρός")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Βροχερός")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Καταιγίδα")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Λιακάδα")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Καύσωνας")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Χιόνι")).inRoot(isPlatformPopup()).perform(click());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Καιρός")).perform(click());
+        Espresso.onView(withText("Σκόνη")).inRoot(isPlatformPopup()).perform(click());
         Espresso.onView(isRoot()).perform(pressBack());
         Espresso.onView(withId(R.id.list_town)).perform(click());
         Espresso.onData(anything()).inAdapterView(withId(R.id.ListView)).atPosition(0).perform(longClick());
