dbcffe425e07ceb48ba9a6c60beaaf59218251d9
npetalid <nikos@petalidis.gr>
2019-12-09 18:07:58 +0200

    Resolved issue #9. Multiple cities are now supported. We now read the city directly from the JSON response.

--- app/src/main/java/com/example/farmweather/BackgroundRequestAPI.java
+++ app/src/main/java/com/example/farmweather/BackgroundRequestAPI.java
@@ -2,18 +2,10 @@ package com.example.farmweather;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.database.Cursor;
-import android.os.AsyncTask;
-import android.os.Build;
-import androidx.annotation.RequiresApi;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class BackgroundRequestAPI extends JobService {
     private boolean jobCancelled;
@@ -22,7 +14,6 @@ public class BackgroundRequestAPI extends JobService {
 
     Calendar rightNow = Calendar.getInstance();
     int currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
-
 
 
     @Override
@@ -36,7 +27,7 @@ public class BackgroundRequestAPI extends JobService {
             @Override
             public void run() {
                 if (jobCancelled) return;
-                new weatherTask().execute();
+                new WeatherTask(BackgroundRequestAPI.this).execute();
                 jobFinished(params, false);
             }
         }).start();
@@ -48,94 +39,4 @@ public class BackgroundRequestAPI extends JobService {
         return true;
     }
 
-    class weatherTask extends AsyncTask<String, Void, String> {
-        String API2 = "75a1a10887c7350764f93ad239553a90";
-        String City;
-        Cursor cursor;
-
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            System.out.println("PreExecute");
-        }
-
-        protected String doInBackground(String... args) {
-            String response;
-
-            cursor = database.getCData();
-            if (cursor.getCount() != 0) {
-                while (cursor.moveToNext()) {
-                    City = cursor.getString(1);
-                    response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=metric&appid=" + API2);
-                    myList.add(response);
-                }
-            }
-            return myList.toString();
-        }
-
-        //DHLWSH ANTIKEIMENWN TYPOU JASON
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        @Override
-        protected void onPostExecute(String result) {
-
-            try {
-                JSONObject jsonObj = new JSONObject(result);
-                JSONObject main = jsonObj.getJSONObject("main");
-                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
-
-                Long updatedAt = jsonObj.getLong("dt");
-                String Time = new SimpleDateFormat("EEE, d MMM yyyy HH:mm", Locale.ENGLISH).format(new Date(updatedAt * 1000));
-                String weatherDescription = weather.getString("main");
-                String tempMin = main.getString("temp_min");
-                String tempMax = main.getString("temp_max");
-
-
-                //METATROPH THERMOKRASIWN SE FLOAT GIA NA TA METATREPSOUME SE AKERAIOUS
-                double minD = Double.parseDouble(tempMin);
-                double maxD = Double.parseDouble(tempMax);
-                float minF = (float) minD;
-                float maxF = (float) maxD;
-
-
-                //METABLHTH GIA METATROPH KAIROU APO AGGLIKA SE ELLHNIKA
-                String WeatherGR = null;
-
-                if(weatherDescription.equals("Clouds")){
-                    WeatherGR = "Καθαρός";
-                }
-                if(weatherDescription.equals("Drizzle") ||
-                        weatherDescription.equals("Rain")){
-                   WeatherGR = "Βροχερός";
-                }
-                if(weatherDescription.equals("Thunderstorm")){
-                   WeatherGR = "Καταιγίδα";
-                }
-                if((weatherDescription.equals("Clear") ||
-                        weatherDescription.equals("Mist") ||
-                        weatherDescription.equals("Fog") ||
-                        weatherDescription.equals("Haze")) && (currentHour >= 19 || currentHour < 6)){
-                    WeatherGR = "Καθαρός";
-                }
-                if((weatherDescription.equals("Clear") ||
-                        weatherDescription.equals("Mist")||
-                        weatherDescription.equals("Fog") ||
-                        weatherDescription.equals("Haze"))&& (currentHour >= 6 && currentHour < 19)) {
-                    WeatherGR = "Λιακάδα";
-                }
-                if(weatherDescription.equals("Snow")){
-                    WeatherGR = "Χιόνι";
-                }
-                if(weatherDescription.equals("Smoke")){
-                    WeatherGR = "Καπνός";
-                }
-                database.insertPData(Time,String.format("%.0f",minF),String.format("%.0f",maxF),WeatherGR,City);
-                System.out.println("OnPostExecute");
-            } catch (JSONException e) {
-                System.out.println(e);
-
-                return;
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/farmweather/WeatherTask.java
@@ -0,0 +1,123 @@
+package com.example.farmweather;
+
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.farmweather.domain.TemperatureReading;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherTask extends AsyncTask<String, Void, List<String>> {
+    private BackgroundRequestAPI backgroundRequestAPI;
+    String API2 = "75a1a10887c7350764f93ad239553a90";
+    String City;
+    Cursor cursor;
+
+    public WeatherTask(BackgroundRequestAPI backgroundRequestAPI) {
+        this.backgroundRequestAPI = backgroundRequestAPI;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        System.out.println("PreExecute");
+    }
+
+    protected List<String> doInBackground(String... args) {
+        String response;
+
+        cursor = backgroundRequestAPI.database.getCData();
+        if (cursor.getCount() != 0) {
+            while (cursor.moveToNext()) {
+                City = cursor.getString(1);
+                response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=metric&appid=" + API2);
+                backgroundRequestAPI.myList.add(response);
+            }
+        }
+        return backgroundRequestAPI.myList;
+    }
+
+    //DHLWSH ANTIKEIMENWN TYPOU JASON
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onPostExecute(List<String> result) {
+
+        List<TemperatureReading> readings = getReadings(result,(backgroundRequestAPI.currentHour >= 19 || backgroundRequestAPI.currentHour < 6));
+        for (TemperatureReading reading:readings) {
+            backgroundRequestAPI.database.insertPData(reading.getTime(), reading.getMin(), reading.getMax(), reading.getDescription(), reading.getCity());
+        }
+    }
+
+    public List<TemperatureReading> getReadings(List<String> results, boolean isNight) {
+        List<TemperatureReading> answers = new ArrayList<>();
+        for (String result:results ) {
+            answers.add(getReading(result,isNight));
+        }
+        return answers;
+    }
+
+    public TemperatureReading getReading(String result, boolean isNight) {
+        try {
+            JSONObject jsonObj = new JSONObject(result);
+            JSONObject main = jsonObj.getJSONObject("main");
+            JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+            Long updatedAt = jsonObj.getLong("dt");
+            String weatherDescription = weather.getString("main");
+            String tempMin = main.getString("temp_min");
+            String tempMax = main.getString("temp_max");
+
+            String city = jsonObj.getString("name");
+
+            //METATROPH THERMOKRASIWN SE FLOAT GIA NA TA METATREPSOUME SE AKERAIOUS
+            double minD = Double.parseDouble(tempMin);
+            double maxD = Double.parseDouble(tempMax);
+            float minF = (float) minD;
+            float maxF = (float) maxD;
+
+
+            //METABLHTH GIA METATROPH KAIROU APO AGGLIKA SE ELLHNIKA
+            String WeatherGR = null;
+
+            if (weatherDescription.equals("Clouds")) {
+                WeatherGR = "Καθαρός";
+            }
+            if (weatherDescription.equals("Drizzle") ||
+                    weatherDescription.equals("Rain")) {
+                WeatherGR = "Βροχερός";
+            }
+            if (weatherDescription.equals("Thunderstorm")) {
+                WeatherGR = "Καταιγίδα";
+            }
+            if ((weatherDescription.equals("Clear") ||
+                    weatherDescription.equals("Mist") ||
+                    weatherDescription.equals("Fog") ||
+                    weatherDescription.equals("Haze")) && isNight) {
+                WeatherGR = "Καθαρός";
+            }
+            if ((weatherDescription.equals("Clear") ||
+                    weatherDescription.equals("Mist") ||
+                    weatherDescription.equals("Fog") ||
+                    weatherDescription.equals("Haze")) && !isNight) {
+                WeatherGR = "Λιακάδα";
+            }
+            if (weatherDescription.equals("Snow")) {
+                WeatherGR = "Χιόνι";
+            }
+            if (weatherDescription.equals("Smoke")) {
+                WeatherGR = "Καπνός";
+            }
+            return new TemperatureReading(updatedAt, String.format("%.0f", minF), String.format("%.0f", maxF), WeatherGR, city);
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Unable to read json object:" + result + " " + e.getMessage());
+        }
+    }
+}
